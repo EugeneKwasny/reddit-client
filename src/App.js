36848ef085bcae16078982aa41df43afad668b23
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2022-12-03 18:00:44 +0200

    Add homepage markup

--- src/App.js
+++ src/App.js
@@ -1,56 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import logo from './images/logo/default.svg';
+import iconSearch from './images/icon/search.svg'
+import logoSmall from './images/logo/small.svg';
+import postThumnailDesktop from './images/post_thumbnail/desktop.png'
+import iconPlus from './images/icon/plus.svg';
+import iconMinus from './images/icon/minus.svg';
+import postThumbnailCircle from './images/post_thumbnail/circle.svg';
 
 function App() {
+  const likesCounter = '37';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+      <header>
+        <div>
+          <img src={logo} />
+        </div>
+        <div>
+        <form>
+          <input type="search" name="search" value="" />
+          <button name="button"><img src={iconSearch} /></button>
+        </form>
+        </div>
       </header>
+      <main>
+        <section>
+          <article>
+            <h3>Musk reinstates Trump's suspended Twitter account following poll</h3>
+            <a href="#">
+              <img src={postThumnailDesktop} />
+            </a>
+            <div>
+              <div>
+                <button><img src={iconPlus} /></button>
+                <span>{likesCounter}</span>
+                <button><img src={iconMinus} /></button>
+              </div>
+            </div>
+          </article>
+          <article>
+            <h3>Musk reinstates Trump's suspended Twitter account following poll</h3>
+            <a href="#">
+              <img src={postThumnailDesktop} />
+            </a>
+            <div>
+              <div>
+                <button><img src={iconPlus} /></button>
+                <span>{likesCounter}</span>
+                <button><img src={iconMinus} /></button>
+              </div>
+            </div>
+          </article>
+          <article>
+            <h3>Musk reinstates Trump's suspended Twitter account following poll</h3>
+            <a href="#">
+              <img src={postThumnailDesktop} />
+            </a>
+            <div>
+              <div>
+                <button><img src={iconPlus} /></button>
+                <span>{likesCounter}</span>
+                <button><img src={iconMinus} /></button>
+              </div>
+            </div>
+          </article>
+        </section>
+        <aside>
+
+          <aside>
+              <div>Subreddits</div>
+              <div>
+                <ul>
+                  <li>
+                    <img src={postThumbnailCircle} />
+                    <h3>r/WhitePeopleTwitter</h3>
+                  </li>
+                  <li>
+                    <img src={postThumbnailCircle} />
+                    <h3>r/WhitePeopleTwitter</h3>
+                  </li>
+                  <li>
+                    <img src={postThumbnailCircle} />
+                    <h3>r/WhitePeopleTwitter</h3>
+                  </li>
+                </ul>
+              </div>
+          </aside>
+        </aside>
+      </main>
+      <footer>
+        <div>
+          <img src={logoSmall} />
+        </div>
+        <div>
+         2022 &copy; All rights reserved
+        </div>
+      </footer>
     </div>
   );
 }
