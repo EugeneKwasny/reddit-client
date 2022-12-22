34b509a01e3d6eebc09e2b8461c7b92277482e1b
Eugene Kvasnyi <evghenii.cvasnii@boosta.co>
2022-12-22 14:59:20 +0200

    Add header styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
 import './App.css';
+import { css } from '@emotion/react'
 
 
 import logo from './images/logo/default.svg';
@@ -21,18 +23,52 @@ function App() {
   const likesCounter = '37';
   return (
     <div className="App">
-    <header>
-    <div>
+    <header css={css`
+      display: flex;
+      column-gap: 20px;
+      margin: 0 auto;
+      padding: 12px 32px;
+      justify-content: space-between;
+      background-color: white;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.05);
+    `}>
+    <div css={css`
+      width:50px; 
+      overflow:clip;
+    `}>
       <Link to={'/'}>
         <img src={logo} />
       </Link>
     </div>
-    <div>
-    <form>
-      <input type="search" name="search" defaultValue="" />
-      <button name="button"><img src={iconSearch} /></button>
+    <form 
+      css={css`
+        position: relative;
+        display: flex;
+        align-items: center;
+        max-width: 293px;
+        width: 100%;
+      `}>
+      <input type="search" name="search" defaultValue="" placeholder='Search'
+      css={css`
+        padding: 10px 15px;
+        background: #EFF5F5;
+        border-radius: 4px;
+        border:none;
+        font-size: 12px;
+        line-height: 24px;
+        width: 100%;
+      `}
+      />
+      <button name="button"
+      css={css`
+        position: absolute;
+        border:none;
+        right:0;
+        background-color: transparent;
+        display: flex;
+      `}
+      ><img src={iconSearch} /></button>
     </form>
-    </div>
   </header>
   <main>
         <Outlet />
